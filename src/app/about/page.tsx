cabf80e19fe7c9d724ceccc7d3217f185552ae08
rahulbrandimpetus <rahul@brandimpetus.com>
2024-07-02 18:43:23 +0530

    about page changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,8 @@
-import AboutPage from "@/components/aboutUs/AboutPage";
+
 import AboutHero from "@/components/aboutUs/HeroSection";
-import LivePlatforms from "@/components/aboutUs/LivePlatforms";
+
 import OurSponsor from "@/components/aboutUs/OurSponsor";
-import OurTeam from "@/components/aboutUs/OurTeam";
-import SponsorsTicker from "@/components/aboutUs/SponsorsTicker";
+
 import WeAreLiveAt from "@/components/aboutUs/WeAreLiveAt";
 import WelcomeSlider from "@/components/aboutUs/WelcomeSlider";
 import FooterSeason2 from "@/components/globalComponents/FooterSeason2";
@@ -22,7 +21,7 @@ const Page = () => {
     <NavbarSeason2/>
   <AboutHero/>
   <WelcomeSlider/>
-  <OurTeam/>
+
 <OurSponsor/>
 <WeAreLiveAt/>
   <FooterSeason2/>
