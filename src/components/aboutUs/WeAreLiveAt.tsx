271495c002bf637e714da5ebf48a3a09ce4f483e
rahulbrandimpetus <rahul@brandimpetus.com>
2024-07-03 17:32:53 +0530

    fixes in about us and esport json

--- src/components/aboutUs/WeAreLiveAt.tsx
+++ src/components/aboutUs/WeAreLiveAt.tsx
@@ -1,3 +1,4 @@
+
 import Link from "next/link";
 
 const WeAreLiveAt = () =>{
@@ -10,9 +11,9 @@ const WeAreLiveAt = () =>{
 
             <div className="flex flex-row flex-wrap justify-around w-full md:gap-71 gap-7 mb-8 xl:px-44 md:px-12 text-white">
                 <Link className="hover:border-[#282828]  border-black border-2 p-4 rounded-lg" href="https://live.glance.com/discover/trends/" target="_blank">   <img src="/live1.svg" alt="" className="md:w-auto w-20"/></Link>
-                <Link className="hover:border-[#282828]  border-black border-2 p-4 rounded-lg" href="https://www.youtube.com/@CollegeRivalsIn/featured" target="_blank">   <img src="/live2.svg" alt="" className="md:w-auto w-20"/></Link>
-                <Link className="hover:border-[#282828]  border-black border-2 p-4 rounded-lg" href="https://www.twitch.tv/collegerivalsin" target="_blank">   <img src="/live3.svg" alt="" className="md:w-auto w-20"/></Link>
-                <Link className="hover:border-[#282828]  border-black border-2 p-4 rounded-lg" href="" target="_blank">   <img src="/live4.svg" alt="" className="md:w-auto w-20"/></Link>
+                <Link className="hover:border-[#282828]  border-black border-2 p-4 rounded-lg flex items-center justify-center" href="https://www.youtube.com/@CollegeRivalsIn/featured" target="_blank">   <img  src="/live2.png" alt=""  className="md:w-auto w-20"/></Link>
+                <Link className="hover:border-[#282828]  border-black border-2 p-4 rounded-lg" href="https://www.twitch.tv/collegerivalsin" target="_blank">   <img src="/live3.svg" alt=""  className="md:w-auto w-20"/></Link>
+                <Link className="hover:border-[#282828]  border-black border-2 p-4 rounded-lg" href="https://jiogames.page.link/E65jtv8CTb7nuAWKA?rf=10313789424998215" target="_blank">   <img src="/live4.svg" alt="" className="md:w-auto w-20"/></Link>
                 <Link className="hover:border-[#282828]  border-black border-2 p-4 rounded-lg" href=" https://kick.com/collegerivalsin" target="_blank">   <img src="/live5.svg" alt="" className="md:w-auto w-20"/></Link>
                 <Link className="hover:border-[#282828]  border-black border-2 p-4 rounded-lg" href=" https://www.facebook.com/collegerivalsin/" target="_blank">   <img src="/live6.svg" alt="" className="md:w-auto w-20"/></Link>
                 <Link className="hover:border-[#282828]  border-black border-2 p-4 rounded-lg" href="  https://rooter.app.link/ZKy7Nv0J1Cb" target="_blank">   <img src="/live7.webp" alt="" className="md:w-48 w-20"/></Link>
