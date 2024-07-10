36c190d18121b60bb10ea8763b39704d958a7df9
Riyasree <riyasree@brandimpetus.com>
2024-07-11 01:12:39 +0530

    Code clean up

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,6 @@
-// 'use client';
-
-import Faq from '@/components/homePage/faq';
-
-import Graphics from '@/components/globalComponents/Graphics';
-import { Flex } from '@chakra-ui/react';
-import { useState } from 'react';
-import BasicDetailsForm from '@/components/sign-up/BasicDetailsForm';
 import LoginForm from '@/components/login/LoginForm';
 
 export default function Login() {
-  //   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <LoginForm />
