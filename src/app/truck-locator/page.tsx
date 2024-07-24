3ead70877fe31404ca1cea7cde43b03ab28d49fe
rahulbrandimpetus <rahul@brandimpetus.com>
2024-07-24 11:47:00 +0530

    truck locator lottify implement

---
+++ src/app/truck-locator/page.tsx
@@ -0,0 +1,19 @@
+import Schedule from "@/components/esports/Schedule"
+import FooterSeason2 from "@/components/globalComponents/FooterSeason2"
+import NavbarSeason2 from "@/components/globalComponents/NavbarSeason2"
+import Truckgallery from "@/components/truck-locator/gallery-truck"
+import Trackhero from "@/components/truck-locator/hero-tuck"
+
+const page = ()=>{
+    return(
+        <div>
+<NavbarSeason2/>
+        <Trackhero/>
+        <Schedule/>
+        <Truckgallery/>
+        <FooterSeason2/>
+        </div>
+     
+    )
+}
+export default page
---
+++ src/components/truck-locator/hero-tuck.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+import LottieAnimation from "./lottie-animation";
+
+
+const Trackhero = () =>{
+
+    return(
+        <div id="truck-locator" className="flex items-center justify-center w-full md:pt-44 pt-5 bg-black md:pb-28">
+        <div className="w-full flex flex-col items-center justify-center max-w-[1440] ">
+
+          <LottieAnimation/>
+        </div>
+        </div>
+    )
+}
+export default Trackhero;
