b672aad98acd70f304bf9ebfc7dbcecefc4dc0b0
rahulbrandimpetus <rahul@brandimpetus.com>
2024-07-26 11:51:33 +0530

    press page fix updates

--- src/app/press/page.tsx
+++ src/app/press/page.tsx
@@ -1,7 +1,7 @@
 import FooterSeason2 from "@/components/globalComponents/FooterSeason2"
 import NavbarSeason2 from "@/components/globalComponents/NavbarSeason2"
 import Contenthub from "@/components/press/content-hub"
-import Galleryhub from "@/components/press/gallery0hub"
+import Galleryhub from "@/components/press/galleryhub"
 import Newshub from "@/components/press/new-hub"
 
 
