a617bead48c85f3bf4fdf3445a4aa691313157e8
Riyasree <riyasree@brandimpetus.com>
2024-07-27 16:47:53 +0530

    Updated nextjs config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.facebook.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.facebook.com',
+        port: '',
+        pathname: '/**',
+      },
+    ]
 },
 };
 
