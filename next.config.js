c41461dba340d0bc0dcfdefa9d988bd5285c4c46
Riyasree <riyasree@brandimpetus.com>
2024-07-31 01:54:52 +0530

    WIP- Updated profile photo url logic, added loader, testing is pending

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains:['college-rivals-prod.s3.ap-south-1.amazonaws.com'],
     remotePatterns: [
       {
+        protocol: 'https',
+        hostname: 'www.facebook.com',
+        port: '',
+        pathname: '/**',
+      }, {
         protocol: 'https',
         hostname: 'www.facebook.com',
         port: '',
