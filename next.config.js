08f395dbc77836fd454c3296ce22bd4587d6829f
Riyasree <riyasree@brandimpetus.com>
2024-08-01 17:45:21 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains:['college-rivals-prod.s3.ap-south-1.amazonaws.com'],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'www.facebook.com',
-        port: '',
-        pathname: '/**',
-      }, {
-        protocol: 'https',
-        hostname: 'www.facebook.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "www.facebook.com",
+        port: "",
+        pathname: "/**",
       },
-    ]
-},
+      {
+        protocol: "https",
+        hostname: "www.facebook.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "college-rivals-prod.s3.ap-south-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
