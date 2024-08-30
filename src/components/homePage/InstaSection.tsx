c9c0d18677f35dc5079f0b1e08ff5ea1ef4d5404
rahulbrandimpetus <rahul@brandimpetus.com>
2024-08-30 18:16:28 +0530

    electric blazeinsta implementation

--- src/components/homePage/InstaSection.tsx
+++ src/components/homePage/InstaSection.tsx
@@ -1,28 +1,13 @@
-"use client";
-import { useEffect } from "react";
-
 const InstaSection = () => {
-  useEffect(() => {
-    // Load the LightWidget script
-    const script = document.createElement("script");
-    script.src = "https://cdn.lightwidget.com/widgets/lightwidget.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
   return (
     <div className="flex justify-center items-center w-full bg-[#1B191A] pt-9 pb-3  md:pt-20 md:pb-28">
       <div className="w-full flex flex-col justify-center items-center max-w-[1440px] px-5">
         <h2 className="uppercase text-2xl md:text-6xl text-white italic ppFormula-font">
           Follow us on <span className="text-#E7327C">insta</span>
         </h2>
-        <div className="w-full md:w-[85%] pt-6 md:pt-16">
-          <iframe
-            src="//lightwidget.com/widgets/f55700f94a9256089e2d328c439d73e0.html"
-            scrolling="no"
-            allowTransparency
-            className="lightwidget-widget"
-            style={{ width: "100%", border: 0, overflow: "hidden" }}
-          ></iframe>
+        <div className="w-full pt-6 md:pt-16">
+          <script src="https://s.electricblaze.com/widget.js" defer></script>
+          <div className="electricblaze-id-2UhxD87"></div>
         </div>
       </div>
     </div>
