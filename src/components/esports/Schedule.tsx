c226c38be973e401a782bde4456bfba5352d3887
Riyasree <riyasree@brandimpetus.com>
2024-09-26 00:13:57 +0530

    updated schedule

--- src/components/esports/Schedule.tsx
+++ src/components/esports/Schedule.tsx
@@ -5,8 +5,8 @@ const Schedule = () =>{
 <div className="w-full bg-black flex items-center justify-center">
         <div>
             <div><h3 className="md:text-6xl text-2xl ppFormula-font mb-8 md:mb-20 text-white italic text-center">SCHEDULE</h3></div>
-            <Image src="/schedule-desktop.png" className="xl:block hidden"  width={870} height={2003} alt="" />
-            <Image src="/schedule-mobile.png" className="xl:hidden block" width={312} height={4998} alt="" />
+            <Image src="/schedule-desktop.webp" className="xl:block hidden"  width={870} height={2003} alt="schedule-desktop.webp" />
+            <Image src="/schedule-mobile.webp" className="xl:hidden block" width={312} height={4998} alt="schedule-mobile.webp" />
         </div>
         </div>
     )
